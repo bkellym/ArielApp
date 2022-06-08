f807e0ecbac0b670f0c838453d3d75de192a5db3
keller.maciel <keller.maciel@sankhya.com.br>
2022-06-07 21:13:23 -0300

    IMP202202 - Configurações e integração com Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'package:ariel_app/screens/homepage.dart';
 import 'package:ariel_app/screens/calendar.dart';
 
 void main() {
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ArielApp());
 }
 
