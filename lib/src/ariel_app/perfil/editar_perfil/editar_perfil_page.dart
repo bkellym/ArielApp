63d48dba011edeccec50498382fc99dd2f1833ad
keller.maciel <keller.maciel@sankhya.com.br>
2022-07-14 23:54:59 -0300

    Ajusta formulário de edição de perfil, limpando o código.

--- lib/src/ariel_app/perfil/editar_perfil/editar_perfil_page.dart
+++ lib/src/ariel_app/perfil/editar_perfil/editar_perfil_page.dart
@@ -26,11 +26,6 @@ class _EditarPerfilPageState extends State<EditarPerfilPage> {
   final EditarPerfilBloc _bloc = EditarPerfilBloc();
   final double leftPadding = 32;
 
-  final EdgeInsetsGeometry campoPadding = const EdgeInsets.only(
-    top: 12,
-    bottom: 6,
-  );
-
   @override
   void initState() {
     _dadosCarregados = _bloc.init(widget.user);
@@ -48,110 +43,72 @@ class _EditarPerfilPageState extends State<EditarPerfilPage> {
               subTitulo: const ['Editar', ' perifl'],
               color: ArielColors.secundary,
               imgFundo: Image.asset('assets/images/ciclos.png').image,
-              child: Material(
-                color: Colors.white,
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CampoImagem(
+                      imagemAtual: widget.user.foto,
+                      imagemNova: _bloc.controller.foto,
+                      onChange: (foto) {
+                        _bloc.controller.foto = foto;
+                      },
+                    ),
+                    CampoTexto(
+                        label: "NOME",
+                        controller: _bloc.controller.nome,
+                        color: ArielColors.secundary),
+                    CampoData(
+                        label: "DATA DE NASCIMENTO",
+                        controller: _bloc.controller.dtNascimento),
+                    CampoDropdown(
+                        label: "GÊNERO",
+                        items: _bloc.listaGeneros,
+                        controller: _bloc.controller.genero),
+                    CampoTexto(
+                        label: "HISTORIA",
+                        maxLines: 4,
+                        color: ArielColors.secundary,
+                        controller: _bloc.controller.historia),
+                    DivisoriaDecorada(
+                      titulo: "SUA CONTA",
+                      cor: ArielColors.secundary,
+                      padding: EdgeInsets.symmetric(
+                        vertical:
+                            SizeConfig.of(context).dynamicScaleSize(size: 16),
+                      ),
+                    ),
+                    CampoTexto(
+                        label: "LOGIN",
+                        controller: _bloc.controller.email,
+                        color: ArielColors.secundary),
+                    CampoTexto(
+                        label: "SENHA ATUAL",
+                        obscureText: true,
+                        color: ArielColors.secundary,
+                        controller: _bloc.controller.senhaAtual),
+                    CampoTexto(
+                        label: "NOVA SENHA",
+                        obscureText: true,
+                        color: ArielColors.secundary,
+                        controller: _bloc.controller.senhaNova),
+                    BotaoPadrao(
+                        label: "SALVAR ALTERAÇÕES",
+                        height:
+                            SizeConfig.of(context).dynamicScaleSize(size: 40),
                         padding: EdgeInsets.only(
-                          right: SizeConfig.of(context).dynamicScaleSize(
-                            size: leftPadding,
-                          ),
-                          left: SizeConfig.of(context).dynamicScaleSize(
-                            size: leftPadding,
-                          ),
-                        ),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              CampoImagem(
-                                imagemAtual: widget.user.foto,
-                                imagemNova: _bloc.controller.foto,
-                                onChange: (foto) {
-                                  _bloc.controller.foto = foto;
-                                },
-                              ),
-                              CampoTexto(
-                                  label: "NOME",
-                                  controller: _bloc.controller.nome,
-                                  color: ArielColors.secundary,
-                                  inputPadding: campoPadding),
-                              CampoData(
-                                  label: "DATA DE NASCIMENTO",
-                                  controller: _bloc.controller.dtNascimento,
-                                  inputPadding: campoPadding),
-                              CampoDropdown(
-                                  label: "GÊNERO",
-                                  items: _bloc.listaGeneros,
-                                  controller: _bloc.controller.genero,
-                                  inputPadding: campoPadding),
-                              CampoTexto(
-                                  label: "HISTORIA",
-                                  maxLines: 4,
-                                  color: ArielColors.secundary,
-                                  controller: _bloc.controller.historia,
-                                  inputPadding: campoPadding),
-                            ]),
-                      ),
-                      DivisoriaDecorada(
-                        titulo: "SUA CONTA",
-                        cor: ArielColors.secundary,
-                        padding: EdgeInsets.symmetric(
-                          vertical:
-                              SizeConfig.of(context).dynamicScaleSize(size: 16),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                          right: SizeConfig.of(context).dynamicScaleSize(
-                            size: leftPadding,
-                          ),
-                          left: SizeConfig.of(context).dynamicScaleSize(
-                            size: leftPadding,
-                          ),
-                        ),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              CampoTexto(
-                                  label: "LOGIN",
-                                  controller: _bloc.controller.email,
-                                  color: ArielColors.secundary,
-                                  inputPadding: campoPadding),
-                              CampoTexto(
-                                  label: "SENHA ATUAL",
-                                  obscureText: true,
-                                  color: ArielColors.secundary,
-                                  controller: _bloc.controller.senhaAtual,
-                                  inputPadding: campoPadding),
-                              CampoTexto(
-                                  label: "NOVA SENHA",
-                                  obscureText: true,
-                                  color: ArielColors.secundary,
-                                  controller: _bloc.controller.senhaNova,
-                                  inputPadding: campoPadding),
-                              BotaoPadrao(
-                                  label: "SALVAR ALTERAÇÕES",
-                                  height: SizeConfig.of(context)
-                                      .dynamicScaleSize(size: 40),
-                                  padding: EdgeInsets.only(
-                                      top: SizeConfig.of(context)
-                                          .dynamicScaleSize(size: 32)),
-                                  textStyle: TextStyle(
-                                      fontSize: SizeConfig.of(context)
-                                          .dynamicScaleSize(size: 12),
-                                      fontWeight: Weight.bold),
-                                  internalPadding: SizeConfig.of(context)
-                                      .dynamicScaleSize(size: 8),
-                                  onPressed: () {
-                                    _bloc.controller.alterar();
-                                    Navigator.pop(context);
-                                  }),
-                            ]),
-                      ),
-                    ]),
-              ),
+                            top: SizeConfig.of(context)
+                                .dynamicScaleSize(size: 32)),
+                        textStyle: TextStyle(
+                            fontSize: SizeConfig.of(context)
+                                .dynamicScaleSize(size: 12),
+                            fontWeight: Weight.bold),
+                        internalPadding:
+                            SizeConfig.of(context).dynamicScaleSize(size: 8),
+                        onPressed: () {
+                          _bloc.controller.alterar();
+                          Navigator.pop(context);
+                        }),
+                  ]),
             );
           } else {
             return const Center(
